f2d78dc36a1dc0dfbcd817ee6089c1bd800fe4e4
rizzhal <barmanbishal1234@gmail.com>
2026-06-28 11:47:36 +0530

    Improvements

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const userName_el = document.getElementById("username")
-const serachBtn_el = document.getElementById("searchBtn")
+const searchBtn_el = document.getElementById("searchBtn")
 const avatar_el = document.getElementById("avatar")
 const name_el = document.getElementById("name")
 const userNameText_el = document.getElementById("usernameText")
@@ -10,25 +10,62 @@ const repos_el = document.getElementById("repos")
 const location_el = document.getElementById("location")
 const company_el = document.getElementById("company")
 const profileLink_el = document.getElementById("profileLink")
-
+const repoContainer_el = document.getElementById("repoContainer")
 
 
 
 async function getData() {
-    const username = userName_el.value
+    const username = userName_el.value.trim()
+    if(username === ""){
+        alert("Please enter a username")
+        return;
+    }
     const uri = `https://api.github.com/users/${username}`
     
     try{
+        searchBtn_el.textContent = "Searching"
+        searchBtn_el.disabled = true
         const response = await fetch(uri)
-        if(!response){
+        if(!response.ok){
             throw new Error('Http error', response.status)
         }
 
         const data = await response.json();
         console.log(data)
+        searchBtn_el.textContent = "search"
+        searchBtn_el.disabled = false
+
+        const avatar = data?.avatar_url??null
+        const name = data?.name??null
+        const usernameText = data?.login??null
+        const bio = data?.bio??null || "Not available"
+        const followers = data?.followers??null
+        const following = data?.following??null
+        const repos = data?.public_repos??null
+        const location = data?.location??null
+        const company = data?.company??null || "Not available"
+        const profileLink = data?.html_url??null
+    
+
+        return{avatar,name,usernameText, bio, followers, following, repos, location, company, profileLink}
     } catch(e){
         console.error('Error fetching data',e)
     }
 }
 
-serachBtn_el.addEventListener("click" , getData)
+searchBtn_el.addEventListener("click" , async() => {
+    const result = await getData()
+    if(!result) return;
+    
+    avatar_el.src= result.avatar
+    name_el.textContent = result.name
+    userNameText_el.textContent = result.usernameText
+    bio_el.textContent = result.bio
+    followers_el.textContent = result.followers
+    following_el.textContent = result.following
+    repos_el.textContent = result.repos
+    location_el.textContent = result.location
+    company_el.textContent = result.company
+    profileLink_el.href = result.profileLink
+    searchBtn_el
+})
